87cacce388c56b586fa38ab7fff503ee0debde7d
Matthew Plastrik <mplas@bu.edu>
2023-05-23 18:19:08 -0400

    added inline css as there is only one style that will be used for the page

--- main.py
+++ main.py
@@ -76,10 +76,16 @@ def home():
 
 
         return f'''
+    
                 <form method="POST">
                     <label>Create a new role (format: name | description):</label><br>
                     <textarea id="role_input" name="role_input" rows="2" cols="50"></textarea><br>
-                    <button type="submit" name="button_text" value="create">Create Role</button><br><br>
+                    <button 
+                        style = ""
+                        type="submit" 
+                        name="button_text" 
+                        value="create">Create Role
+                    </button><br><br>
 
                     <label>Enter some text:</label><br>
                     <textarea id="text_input" name="text_input" rows="5" cols="50"></textarea><br>
@@ -100,10 +106,16 @@ def home():
             '''
 
     return f'''
+        
         <form method="POST">
             <label>Create a new role (format: name | description):</label><br>
             <textarea id="role_input" name="role_input" rows="5" cols="50"></textarea><br>
-            <button type="submit" name="button_text" value="create">Create Role</button><br><br>
+           <button 
+                style = ""
+                type="submit" 
+                name="button_text" 
+                value="create">Create Role
+            </button><br><br>
             <label>Enter some text:</label><br>
             <textarea id="text_input" name="text_input" rows="5" cols="50"></textarea><br>
             <label>Select an option:</label><br>
