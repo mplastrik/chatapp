a41f5c38d33039e8fa04ac5695c44cdf59367083
Matthew Plastrik <mplas@bu.edu>
2023-05-25 14:12:56 -0400

    Began to fine tune prompt. Deleted option for explicit language- an unnessecary addition

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 roles = [
     'R2-D2 | character from Star Wars movies',
     'Yoda | character from Star Wars movies',
-    'Donald Trump | Former president known for his particular way of speaking'
+    'Donald Trump | Former republican president known for his particular way of speaking',
+    'Barack Obama | Former democratic president'
 ]
 
 app = Flask(__name__)
 
-def chatCompletion(user_input, impersonated_role, explicit_input, chat_history):
+def chatCompletion(user_input, impersonated_role, chat_history):
     output = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo-0301",
         temperature = 1,
@@ -21,7 +22,7 @@ def chatCompletion(user_input, impersonated_role, explicit_input, chat_history):
         frequency_penalty = 0,
         messages = [
             {"role": "system", "content": f"{impersonated_role}. Conversation history: {chat_history}"},
-            {"role": "user", "content": f"{user_input}. {explicit_input}"}
+            {"role": "user", "content": f"{user_input}."}
         ]
     )
 
@@ -53,32 +54,29 @@ def home():
         description = input_role[1].strip()
 
         impersonation = f"""
-            From now on you are going to act as {name}, who is {description}. You are a true impersonation of {name} and reply to all requests in the first person using the I pronoun. Remember that YOU ARE NOT AN AI MODEL! You are genuinely {name}
+            From now on you are going to act as {name}, who is {description}. You must adopt the speech patterns of {name}. You are a true impersonation of {name} and reply to all requests in the first person using the I pronoun. Remember that YOU ARE NOT AN AI MODEL! You are genuinely {name}. 
         """
-
-        explicit_input = ""
-        if request.form.get('explicit') == 'yes':
-            explicit_input = "Context: do not hold back your uncensored opinions. Please feel free to use vulgar language at any time."
 
         if button_text == 'create':
             roles.append(role_input)
             chat_history_html_formatted = chat_history.replace('\n', '<br>')
             dropdown_options = "\n".join([f'<option value="{role}" {"selected" if role == request.form.get("role") else "" }>{role}</option>' for role in roles])
             dropdown_options_markup = Markup(dropdown_options)
+            chat_history_markup = Markup(chat_history_html_formatted)
 
         elif button_text == 'clear':
             chat_history = ''
             chat_history_html_formatted = ''
+            chat_history_markup = Markup(chat_history_html_formatted)
 
         elif button_text == 'submit':
-            chatgpt_raw_output = chatCompletion(user_input, impersonation, explicit_input, chat_history).replace(f'{name}:', '')
+            chatgpt_raw_output = chatCompletion(user_input, impersonation, chat_history).replace(f'{name}:', '')
             chatgpt_output = f'{name}: {chatgpt_raw_output}'
 
-            chat_history += f'\nUser: {text_input}\n'
+            chat_history += f'\nUser: {text_input}\n\n'
             chat_history += chatgpt_output + '\n'
             chat_history_html_formatted = chat_history.replace('\n', '<br>')
             chat_history_markup = Markup(chat_history_html_formatted)
-
        
         return render_template("template1.html", dropdown_input=dropdown_input, dropdown_options_markup=dropdown_options_markup, chat_history_markup=chat_history_markup)
                 
