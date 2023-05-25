2667728e2f6950cd4fdb253b439e4608c5c14d64
Matthew Plastrik <mplas@bu.edu>
2023-05-25 17:36:55 -0400

    created global variable for the chat history so more than just the previous message can be dislayed

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import os
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
+chat_history = ''
+
 roles = [
     'R2-D2 | character from Star Wars movies',
     'Yoda | character from Star Wars movies',
     'Donald Trump | Former republican president known for his particular way of speaking',
     'Barack Obama | Former democratic president'
 ]
+
 
 app = Flask(__name__)
 
@@ -31,23 +34,21 @@ def chatCompletion(user_input, impersonated_role, chat_history):
 
     return chatgpt_output
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
+
     dropdown_options = "\n".join([f'<option value ="{role}">{role}</option>' for role in roles])
     dropdown_options_markup = Markup(dropdown_options)
 
     if request.method == 'POST':
         dropdown_options = "\n".join([f'<option value="{role}" {"selected" if role == request.form.get("role") else "" }>{role}</option>' for role in roles])
-        
-
         button_text = request.form.get('button_text')
         text_input = request.form.get('text_input')
         dropdown_input = request.form.get('role')
-        chat_history = request.form.get('history')
-
         user_input = request.form.get('text_input')
         input_role = request.form.get('role').split('|')
-
+        global chat_history
         role_input = request.form.get('role_input')
 
         name = input_role[0].strip()
@@ -59,25 +60,26 @@ def home():
 
         if button_text == 'create':
             roles.append(role_input)
-            chat_history_html_formatted = chat_history.replace('\n', '<br>')
             dropdown_options = "\n".join([f'<option value="{role}" {"selected" if role == request.form.get("role") else "" }>{role}</option>' for role in roles])
             dropdown_options_markup = Markup(dropdown_options)
-            chat_history_markup = Markup(chat_history_html_formatted)
+        
+            
 
-        elif button_text == 'clear':
+        if button_text == 'clear':
             chat_history = ''
             chat_history_html_formatted = ''
             chat_history_markup = Markup(chat_history_html_formatted)
 
-        elif button_text == 'submit':
+        if button_text == 'submit':
             chatgpt_raw_output = chatCompletion(user_input, impersonation, chat_history).replace(f'{name}:', '')
             chatgpt_output = f'{name}: {chatgpt_raw_output}'
 
-            chat_history += f'\nUser: {text_input}\n\n'
-            chat_history += chatgpt_output + '\n'
+            chat_history += (f'\nUser: {text_input}\n\n')
+            chat_history += (chatgpt_output + '\n')
+           
             chat_history_html_formatted = chat_history.replace('\n', '<br>')
             chat_history_markup = Markup(chat_history_html_formatted)
-       
+
         return render_template("template1.html", dropdown_input=dropdown_input, dropdown_options_markup=dropdown_options_markup, chat_history_markup=chat_history_markup)
                 
             
